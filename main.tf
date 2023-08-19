8747dfe646726c3a39aeb79ad4ba25e9905dbbb8
raghib1992 <65724939+raghib1992@users.noreply.github.com>
2023-08-19 23:22:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "myec2" {
    ami = "ami-0d951b011aa0b2c19"
-   instance_type = lookup(var.instance_type,terraform.workspace)
+   instance_type = var.instance_type
    key_name = "mumbai-key"
 }
